3809450c00c8afa4ee231839c69062f1df5e2b32
enmanuelvielma953-blip <enmanuelvielma953@gmail.com>
2026-08-24 18:57:06 -0300

    api supabase

--- js/supabase.js
+++ js/supabase.js
@@ -1,2 +1,7 @@
 const SUPABASE_URL = 'https://vlzywajkauwzptqdrfgt.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_GGx1dts8SHCPQg9Az4eo5Q_fsB2OyoQ';
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
